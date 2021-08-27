8ce2e58ccfedea0d408a1fc8073c8a79e751b278
jfkriz <jim@kriz.net>
2021-08-27 13:13:51 +0100

    Set timezone, scale, and method

--- src/service/icalGenerator.ts
+++ src/service/icalGenerator.ts
@@ -1,12 +1,18 @@
 import { GSheetReader } from '../repository';
-import { ICalCalendar } from 'ical-generator';
+import { ICalCalendar, ICalCalendarMethod } from 'ical-generator';
+import {getVtimezoneComponent} from '@touch4it/ical-timezones';
 
 export class IcalGenerator {
     async generate(): Promise<String> {
         let cal = new ICalCalendar();
         cal.name('St. Xavier Cross Country Carpool 2021');
-        cal.description('St. Xavier Cross Country Carpool 2021 - Riders and Drivers')
-        cal.timezone('America/New_York');
+        cal.description('St. Xavier Cross Country Carpool 2021 - Riders and Drivers');
+        cal.scale('GREGORIAN');
+        cal.method(ICalCalendarMethod.PUBLISH);
+        cal.timezone({
+            name: 'America/New_York',
+            generator: getVtimezoneComponent
+        });
         
         const events = await new GSheetReader().readEventsFromSheet();
         events.forEach(event => cal.createEvent(event));
